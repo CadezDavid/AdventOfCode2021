bbe7c58204912a386af27afd94f96e7756b17fa0
David Čadež <david.cadez7@gmail.com>
2021-12-01 12:07:17 +0100

    part 2 of day 1

--- src/day1.cpp
+++ src/day1.cpp
@@ -1,33 +1,51 @@
 #include <fstream>
-#include <iostream>
 
 using namespace std;
 
 const char in[] {"in/day1.in"};
 const char out[] {"out/day1.out"};
 
-int main() {
-    ifstream ifs(in);
 
+int part1() {
+    ifstream ifs(in);
     int curr;
     int prev;
-    
     int count {0};
-
     ifs >> prev;
-
-    while (ifs) {
-        ifs >> curr;
+    while (ifs >> curr) {
         if (prev < curr) {
             count++;
-            cout << prev << " -> " << curr << "\n";
-            cout << count << "\n\n";
         }
         prev = curr;
     }
+    return count;
+}
+
+int part2() {
+    ifstream ifs(in);
+    int window[3];
+    int count {0};
+    int curr;
+    ifs >> window[0];
+    ifs >> window[1];
+    ifs >> window[2];
+    while (ifs >> curr) {
+        if (window[0] < curr) {
+            count++;
+        }
+        window[0] = window[1];
+        window[1] = window[2];
+        window[2] = curr;
+    }
+    return count;
+}
+
+int main() {
+    ifstream ifs(in);
 
     ofstream ofs(out);
-    ofs << count;
+    ofs << "part 1: " << part1() << endl;
+    ofs << "part 2: " << part2() << endl;
 
     return 0;
 }
